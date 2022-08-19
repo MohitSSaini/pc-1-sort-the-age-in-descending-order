0c6a9d31dab0306239eed082661d08e6d1887b0f
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-19 11:26:16 +0530

    Sorting age code and test case also check pass

--- src/main/java/com/jap/SortingAge.java
+++ src/main/java/com/jap/SortingAge.java
@@ -1,22 +1,35 @@
 package com.jap;
 
 public class SortingAge {
-
+    
+    public static void main ( String[] args ) {
+        //Declare and initialize an integer array containing age.
+        int[] ageArray = { 22 , 34 , 33 , 32 , 36 , 27 , 28 };
+        
+        //Create an object of the class SortingAge
+        SortingAge sortingAge = new SortingAge ( );
+        
+        //Call the method getSortedAge and pass the parameter
+        int[] sortedArray = sortingAge.getSortedAge ( ageArray );
+        for ( int i = 0 ; i < sortedArray.length ; i++ ) {
+            System.out.print ( sortedArray[ i ] + " " );
+        }
+    }
+    
     //Write the logic to sort the array containing the age in descending order
     // inside the below method and return the sorted array
-    public int[] getSortedAge(int [] ageArray){
-
-        return new int[0];
-
-    }
-
-    public static void main(String[] args) {
-        //Declare and initialize an integer array containing age.
-
-        //Create an object of the class SortingAge
-
-        //Call the method getSortedAge and pass the parameter
-
-
+    public int[] getSortedAge ( int[] ageArray ) {
+        int temp;
+        for ( int i = 0 ; i < ageArray.length ; i++ ) {
+            for ( int j = 0 ; j < ageArray.length - i - 1 ; j++ ) {
+                if ( ageArray[ j ] < ageArray[ j + 1 ] ) {
+                    temp = ageArray[ j ];
+                    ageArray[ j ] = ageArray[ j + 1 ];
+                    ageArray[ j + 1 ] = temp;
+                }
+            }
+            
+        }
+        return ageArray;
     }
 }
